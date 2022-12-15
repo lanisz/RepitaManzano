615170a290c9c5829247b67386e897b46d9eee22
Alanis Araujo <114356922+lanisz@users.noreply.github.com>
2022-12-15 14:15:14 -0300

    Add files via upload

---
+++ E10_DivisaoInteira.java
@@ -0,0 +1,22 @@
+package manzanoRepita;
+
+import java.util.Scanner;
+
+public class E10_DivisaoInteira {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe o valor divisor: ");
+		byte divisor = sc.nextByte();
+		System.out.println("Informe o valor dividendo: ");
+		byte dividendo = sc.nextByte();
+		
+//		do {
+//			
+//		} while();
+
+	}
+
+}
